d81af1ca783cb8ef9f923b27a611d354a210c044
John Lawrence M. Penafiel <penafieljlm@gmail.com>
2015-08-28 17:46:28 +0800

    Removed unecessary descriptions

--- pse-quotes.py
+++ pse-quotes.py
@@ -3,30 +3,6 @@
 """
     Python Script which retrieves PSE stock quotes from the Open PSE Initiative.
     See http://openpse.com/ for more information.
-    
-    Usage
-    ```
-        pse-quotes.py <stocks> --start <YYYY-MM-DD> --end <YYYY-MM-DD>
-    ```
-        
-    Output
-    ```
-        {
-            "status": <status-code>,
-            "data": [
-                {
-                    "name": "<stock-name>",
-                    "price_close": "<closing-price>",
-                    "price_high": "<high-price>",
-                    "price_low": "<low-price>",
-                    "price_open": "<opening-price>",
-                    "quote_date": "<yyyy-mm-dd>",
-                    "symbol": "<stock-symbol>",
-                    "volume": <trade-volume>
-                }
-            ]
-        }
-    ```
 """
 __author__ = "John Lawrence M. Penafiel"
 __copyright__ = "Copyright 2015, John Lawrence M. Penafiel"
